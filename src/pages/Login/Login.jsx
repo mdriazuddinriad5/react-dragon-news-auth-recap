76e7aee73cc364f2ed8a65abcf80349d5e164dce
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-10-04 22:50:08 +0600

    added login route

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h3 className="text-2xl">This is login page</h3>
+        </div>
+    );
+};
+
+export default Login;
